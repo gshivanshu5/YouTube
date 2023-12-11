af784ed8b103006a8cf430dceb78c7a8083597b6
Shivanshu <gkshivanshu@gmail.com>
2023-12-11 11:24:43 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import VideoList from './components/video_list';
+import VideoListItem from './components/video_list_item';
+import Search_bar from './components/search_bar';
+import Video_detail from './components/video_detail';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+      <VideoList />
+      <Search_bar/>
+    </>
   );
 }
 
